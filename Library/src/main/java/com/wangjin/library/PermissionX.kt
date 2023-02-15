7efce20fdcdb2b50cc4f3fe6e488dde81e226519
wangjin <1217611388@qq.com>
2023-02-15 23:42:06 +0800

    three commit

--- Library/src/main/java/com/wangjin/library/PermissionX.kt
+++ Library/src/main/java/com/wangjin/library/PermissionX.kt
@@ -1,17 +1,20 @@
 package com.wangjin.library
 
-import android.app.Activity
 import androidx.fragment.app.FragmentActivity
 
-class PermissionX {
-    companion object {
-        private const val TAG = "InvisibleFragment"
-    }
+object PermissionX {
+    private const val TAG = "PermissionX"
 
-    fun request(activity: FragmentActivity,vararg permissions : String,callback : PermissionCallback){
+    fun request(activity: FragmentActivity,vararg permissions : String,callback : PermissionCallback) {
         val fragmentManager = activity.supportFragmentManager
         val existedFragment = fragmentManager.findFragmentByTag(TAG)
-        
-
+        val fragment = if (existedFragment != null) {
+            existedFragment as InvisibleFragment
+        } else {
+            val invisibleFragment = InvisibleFragment()
+            fragmentManager.beginTransaction().add(invisibleFragment, TAG).commitNow()
+            invisibleFragment
+        }
+        fragment.requestNow(callback, *permissions)
     }
 }
